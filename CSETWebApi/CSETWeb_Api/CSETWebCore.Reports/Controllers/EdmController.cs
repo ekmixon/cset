b626587d466df2206880b3cb445c55645c0a9982
ijohnnmotleyl <john.motley@inl.gov>
2021-09-23 11:52:56 -0600

    Added backend endpoint for edm deficiency reports. Need to find a good way to implement in the frontend, using a hacky workaround atm that I'm not committing.

---
+++ CSETWebApi/CSETWeb_Api/CSETWebCore.Reports/Controllers/EdmController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSETWebCore.Business.Authorization;
+using CSETWebCore.Interfaces.Helpers;
+using CSETWebCore.Reports.Helper;
+using CSETWebCore.Business.Reports;
+using CSETWebCore.Interfaces.Reports;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace CSETWebCore.Reports.Controllers
+{
+    public class EdmController : Controller
+    {
+        private readonly ITokenManager _token;
+        private readonly IReportsDataBusiness _reports;
+        private readonly IViewEngine _engine;
+
+        public EdmController(ITokenManager token, IReportsDataBusiness reports, IViewEngine engine)
+        {
+            _token = token;
+            _reports = reports;
+            _engine = engine;
+        }
+
+        [HttpGet]
+        [CsetAuthorize]
+        [Route("getDeficiencyPdf")]
+        public async Task<IActionResult> CreateDeficiencyReportPdf()
+        {
+            var assessmentId = _token.AssessmentForUser();
+            _reports.SetReportsAssessmentId(assessmentId);
+
+            var reportHtml = await RenderDeficiencyReport();
+
+            var pdfRenderer = new IronPdf.ChromePdfRenderer();
+            var pdf = await pdfRenderer.RenderHtmlAsPdfAsync(reportHtml);
+
+            return File(pdf.BinaryData, "application/pdf", "deficiencyReport.pdf");
+        }
+
+        //TODO: Extract common features for this and CrrController to prevent code reuse
+        private async Task<string> RenderDeficiencyReport()
+        {
+            // View model and data
+            TempData["Links"] = UrlStringHelper.GetBaseUrl(Request);
+            ViewData.Model = new MaturityBasicReportData
+            {
+                DeficienciesList = _reports.GetMaturityDeficiencies(),
+                Information = _reports.GetInformation()
+            };
+
+            // Building the html string
+            await using var sw = new StringWriter();
+            var viewResult = _engine.FindView(ControllerContext, "DeficiencyReport", false);
+            var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData,
+                TempData, sw, new HtmlHelperOptions());
+            await viewResult.View.RenderAsync(viewContext);
+
+            return sw.GetStringBuilder().ToString();
+        }
+    }
+}
